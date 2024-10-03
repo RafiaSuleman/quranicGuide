c183de99dea7ed02936ec4bc2c1efaa65f675a8d
RafiaSuleman <rafiasuleman47@gmail.com>
2024-10-03 17:40:25 +0500

    videos

---
+++ src/app/videos/page.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="grid xl:grid-cols-3 grid-cols-1 gap-4 mx-10">
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video1.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video2.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video3.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video1.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video2.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video3.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video1.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video2.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div>  
+      <video autoPlay loop muted playsInline className="w-full h-64 object-cover rounded-lg shadow-lg">
+        <source src="/videos/video3.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div>
+  
+  
+  )
+}
+
+export default page
